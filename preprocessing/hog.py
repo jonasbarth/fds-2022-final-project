2e53eee0687ed30c53d261c0d4cf92001f0441e0
jonasbarth <jonas.barth.95@gmail.com>
2022-12-11 19:14:42 +0100

    #7 HOG * add a preprocessing package * add a hog module with some TODOs

---
+++ preprocessing/hog.py
@@ -0,0 +1,8 @@
+"""A module with preprocessing functions for the Histogram of Gradients (HOG) pipeline."""
+
+# Preliminary TODOs (not all might stay in this module and/or package.
+# TODO function for downsampling
+# TODO function for applying gaussian
+# TODO function for creating histogram
+# TODO function for saving histograms
+
