724a180f8f742bebd7ab55b5752de98423ed7d2c
jonasbarth <jonas.barth.95@gmail.com>
2022-12-12 20:07:55 +0100

    #7 * add downsampling, gaussian, and hog functions

---
+++ preprocessing/__init__.py
@@ -0,0 +1,1 @@
+from .hog import downsample, apply_gaussian, create_hog
--- preprocessing/hog.py
+++ preprocessing/hog.py
@@ -1,8 +1,29 @@
 """A module with preprocessing functions for the Histogram of Gradients (HOG) pipeline."""
+from skimage.feature import hog
+from skimage.filters import gaussian
+from skimage.transform import resize
 
-# Preliminary TODOs (not all might stay in this module and/or package.
-# TODO function for downsampling
-# TODO function for applying gaussian
-# TODO function for creating histogram
+
+def downsample(image, new_width, new_height):
+    """Returns a downsampled version of the input image with the new width and new height.
+
+    :arg
+    image (np.array) - a numpy array.
+    new_width (int) - the new width of the image
+    new_height (int) the new height of the image
+
+    :return
+    a numpy array of the downsampled image.
+    """
+    return resize(image, (new_width, new_height, image.shape[-1]))
+
+
+def apply_gaussian(image, sigma=1):
+    """Applies a gaussian filter to the image."""
+    return gaussian(image, sigma=sigma)
+
+
+def create_hog(image):
+    """Creates a histogram of gradients from the provided image."""
+    return hog(image, orientations=8, pixels_per_cell=(16, 16), cells_per_block=(1, 1), visualize=True, channel_axis=-1)[1]
 # TODO function for saving histograms
-
