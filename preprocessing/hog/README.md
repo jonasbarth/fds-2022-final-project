45444e94358b29d594309b4b2d2c899f4267ffa4
jonasbarth <jonas.barth.95@gmail.com>
2022-12-14 17:06:53 +0100

    #7 preprocessing script * add command line arguments and logging to main script * add comments * update readme for preprocessing script

--- preprocessing/hog/README.md
+++ preprocessing/hog/README.md
@@ -10,33 +10,46 @@ The package contains the following modules:
 * `pipeline`: contains functions for create scikit pipelines. Useful if you want to have a single pipeline for preprocessing and model fitting.
 * `main`: a script that preprocesses the data.
 
-
 ## Preprocessing Script
 Our image data is > 10GB and is difficult to fit into RAM. For this reason, we created a script that does all the 
 necessary preprocessing in batches and saves the data with its labels, such that it is ready to use for a classifier. 
 After preprocessing, the data footprint is reduced by a factor of 50.
 
 ### Prerequisites
-You need to make sure that the paths specified in the `config.yaml` file point to the folders of your data.
+
+#### 1. Data
+You need to have the image data on your machine.
+
+#### 2. Configuration
+The script relies on a config file to tell it where to find the data, save the histograms, and what parameters to use 
+during the preprocessing. You need to make sure that the paths specified in the `config.yaml` file point to the folders 
+of your data. Fill in the rest of the parameters to your liking.
+   
+Example:
 ```yaml
 source:
-  path:
+  path: # the path where to data will be loaded from.
     dev: data/image/dev
     test: data/image/test
     train: data/image/train
 hog:
-  path:
+  path: # the path where the data will be saved.
     dev: data/hog/dev
     test: data/hog/test
     train: data/hog/train
-  channels:
+  normalising: dataset/channel_mean_std.csv
+  channels: # the different channels that will be used. In the end, there will be one dataset per channel.
     - [1, 2, 3]
     - [6, 7, 8]
     - [11, 12, 13]
 ```
 
 ### How to run
-The script is a simple python script. Run it from the `hog` package.
+The script is a python script that takes two command line arguments:
+- `-c` for the path to the config file.
+- `-m` for the path to the metadata file.
+
+The script needs these two files to be able to run properly. Running it from the root of the project for example:
 ```python
-python main.py
+python -m preprocessing.hog.main -c preprocessing/hog/config.yaml -m dataset/metadata.csv
 ```
--- preprocessing/hog/main.py
+++ preprocessing/hog/main.py
@@ -1,19 +1,32 @@
 """A script for preprocessing image data for a classifier that takes Histogram of Gradients."""
+import argparse
+import logging
+import os
 
 import munch
 import numpy as np
 import pandas as pd
 import yaml
-from tqdm import tqdm
 
 from preprocessing.hog import saving_pipeline
 from util import load_arrays_batch, get_img_file_names
 
 if __name__ == '__main__':
+    # Setting up the logger
+    logging.root.setLevel(logging.NOTSET)
+    logging.basicConfig(format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
 
-    config = munch.munchify(yaml.safe_load(open("./preprocessing/hog/config.yaml")))
+    # Parsing the command line arguments
+    argParser = argparse.ArgumentParser()
+    argParser.add_argument("-c", "--config", help="path of the config to load")
+    argParser.add_argument("-m", "--metadata", help="path of the metadata file to load")
 
-    metadata = pd.read_csv('./dataset/metadata.csv')
+    args = argParser.parse_args()
+
+    # Reading config files
+    config = munch.munchify(yaml.safe_load(open(args.config)))
+
+    metadata = pd.read_csv(args.metadata)
     metadata.img_slice = metadata.img_slice.apply(lambda path: path.split('/')[-1])
 
     normalising = pd.read_csv(config.hog.normalising)
@@ -22,7 +35,13 @@ if __name__ == '__main__':
 
     # go through the dev, test, train, folders.
     for source_path, hog_path in zip(vars(config.source.path).values(), vars(config.hog.path).values()):
+        logging.info(f"Reading image files from {source_path}")
         filenames = get_img_file_names(source_path)
+
+        # create the output directory if it doesn't exist
+        if not os.path.exists(hog_path):
+            logging.info(f"Output path {hog_path} doesn't exist. Creating {hog_path}")
+            os.makedirs(hog_path)
 
         # go through each combination of channels
         for channel in config.hog.channels:
@@ -32,14 +51,18 @@ if __name__ == '__main__':
             file_name_suffix = '_'.join(map(str, channel))
             channel = channel - 1
 
-            for images, names in tqdm(load_arrays_batch(filenames, batch_size=10)):
-                p = saving_pipeline(means, stds, channel, 214, 214, 1, f'{hog_path}/hog_{file_name_suffix}.npy')
+            for images, names in load_arrays_batch(filenames, batch_size=10):
+                data_output_path = f'{hog_path}/hog_{file_name_suffix}.npy'
+                p = saving_pipeline(means, stds, channel, 214, 214, 1, data_output_path)
 
                 transformed = p.fit_transform(images)
+
+                logging.info(f"Saved {data_output_path}.")
 
         # create a labels file for each image folder (dev, test, train).
         labels = []
 
+        logging.info(f'Creating labels for {hog_path}.')
         for filename in filenames:
             filename = filename.split('/')[-1]
 
@@ -47,4 +70,6 @@ if __name__ == '__main__':
             labels.append(label)
 
         labels = np.array(labels)
-        np.save(f'{hog_path}/labels.npy', labels)
+        labels_output_path = f'{hog_path}/labels.npy'
+        np.save(labels_output_path, labels)
+        logging.info(f'Saved {labels_output_path}.')
