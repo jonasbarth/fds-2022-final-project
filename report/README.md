cff6110399c6104673e99259b6041f17cd0c38d5
jonasbarth <jonas.barth.95@gmail.com>
2022-12-16 13:24:52 +0100

    #5 write report * add a channels script that extract channels from an image and saves new images * add README to explain the script.

--- report/README.md
+++ report/README.md
@@ -1,2 +1,16 @@
 # Data For Final Report
 The purpose of this package is to gather notebooks and scripts that generate some data to be used in the final report.
+
+# channels
+The `channels.py` is a script that takes an image as input, extracts specified channels, and saves images with the
+extracted channels.
+
+It has the following flags:
+* `-i` the path to the image.
+* `-c` the channels to extract. Each channel is separated by `,` and each set of channels is separated by `:`.
+* `-o` the output directory. Will be created if it doesn't exist.
+
+Running from the root of the project:
+```bash
+python -m report.channels -i data/image/train/slice_12_img_136.npy -c 1,2,3:6,7,8:11,12,13 -o ./doc
+```
---
+++ report/channels.py
@@ -0,0 +1,58 @@
+"""A script that reads an image and generates one image per channel subset"""
+import argparse
+import logging
+import os
+import sys
+
+import cv2
+import numpy as np
+
+from preprocessing.hog import ChannelSelector
+
+if __name__ == '__main__':
+    # Setting up logging
+    logging.root.setLevel(logging.INFO)
+    logging.basicConfig(format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+
+    # Parsing the command line arguments
+    argParser = argparse.ArgumentParser()
+    argParser.add_argument("-i", "--image", help="path of the image to load")
+    argParser.add_argument("-c", "--channels", help="a list of lists of comma separated channels. Lists are separated with :. Example: 1,2,3:6,7,8:11,12,13")
+    argParser.add_argument("-o", "--output", help="the output directory")
+
+    args = argParser.parse_args()
+
+    image_path = args.image
+
+    if not os.path.exists(image_path):
+        logging.error(f'The provided source path {image_path} does not exist. Image cannot be loaded.')
+        sys.exit(-1)
+
+    image = np.load(image_path)
+
+    logging.info(f'Successfully loaded image from {image_path}')
+
+    channels = args.channels.split(':')
+    channels = map(lambda c: list(map(int, c.split(','))), channels)
+
+    # create the output directory if it doesn't exist
+    if not os.path.exists(args.output):
+        logging.info(f"Output path {args.output} doesn't exist. Creating {args.output}")
+        os.makedirs(args.output)
+
+    for channel in channels:
+        logging.info(f'Extracting channels: {channel} from image.')
+        subset_image = ChannelSelector(channels=channel).fit_transform(image)
+
+        for i in range(len(channel)):
+            min_ = np.min(subset_image[:, :, i])
+            max_ = np.max(subset_image[:, :, i])
+            subset_image[:, :, i] = (subset_image[:, :, i] - min_) / (max_ - min_)
+
+        image_output_name = '_'.join(map(str, channel))
+
+        subset_image = cv2.cvtColor(subset_image, cv2.COLOR_BGR2RGB)
+
+        output_path = f'{args.output}/{image_output_name}.jpg'
+        cv2.imwrite(output_path, subset_image * 255)
+        logging.info(f'Image successfully saved at {output_path}')
